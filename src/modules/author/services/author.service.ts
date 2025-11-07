21a2747aebca53d4d8c032928cdd95bfedaf0a50
naeembs-23 <naeem.imran@brainstation-23.com>
2025-11-07 10:41:11 +0600

    Update: author service

--- src/modules/author/services/author.service.ts
+++ src/modules/author/services/author.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateAuthorDto } from '../dtos/create-author.dto';
 
 @Injectable()
 export class AuthorService {
-  constructor() {}
-  async createAuthor(authorDto: CreateAuthorDto) {
-    return 'This action adds a new author';
+    private logger: Logger;
+    constructor(){
+        this.logger = new Logger(AuthorService.name);
+    }
+  async createAuthor(createAuthorDto: CreateAuthorDto): Promise<string> {
+    this.logger.log('Creating author');
+
+    try{
+        console.log('createAuthorDto: ', createAuthorDto);
+        return 'This action adds a new author'
+    }
+    catch(error){
+        this.logger.error('Error creating author', error);
+        throw error;
+    }
   }
 }
