c32a06cdcfbd1bb5da61a3883647f338e9c2bb00
naeem4265 <md.naeem.al.imran@gmail.com>
2025-11-08 12:48:32 +0600

    Add: database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorModule } from './modules/author/author.module';
+import { ConfigModule } from '@nestjs/config';
+import { getDatabaseConfig } from './config/database.config';
+import { ConfigService } from '@nestjs/config';
+import { envValidationSchema } from './config/env.schema';
 
 @Module({
-  imports: [AuthorModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      validationSchema: envValidationSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: getDatabaseConfig,
+      inject: [ConfigService],
+    }),
+    AuthorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/author/author.module.ts
+++ src/modules/author/author.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Author } from './entities/author.entity';
 import { AuthorController } from './controllers/author.controller';
 import { AuthorService } from './services/author.service';
+import { AuthorRepository } from './repositories/author.repository';
 
 @Module({
-  providers: [AuthorService],
+  imports: [TypeOrmModule.forFeature([Author])],
   controllers: [AuthorController],
+  providers: [AuthorService, AuthorRepository],
+  exports: [AuthorService],
 })
 export class AuthorModule {}
